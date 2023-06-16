9582fd3505fb80b142a9c6b549156a1f4687987e
gonza <gonzalorom2001@gmail.com>
2023-06-16 01:04:51 -0300

    ejerciciosJava

---
+++ src/GuiaNr3/nr55.java
@@ -0,0 +1,10 @@
+/* Realizar una función que reciba dos parámetros, una CADENA de texto y un CARACTER; a partir de estos datos determinar si ese carácter pasado como parámetro se encuentra en la cadena. La función debería devolver la cantidad de veces que se encuentra ese carácter en la cadena.
+Por Ejemplo:
+Cadena = “hola sapo sin cola”
+Si invocamos a la función deberíamos pasarle como parámetro la cadena y luego el carácter que se desea buscar dentro de la cadena; la función debería recorrer la cadena buscando el carácter y contar la cantidad de veces que se encuentra y devolver ese valor. */
+
+package GuiaNr3;
+
+public class nr55 {
+    
+}
